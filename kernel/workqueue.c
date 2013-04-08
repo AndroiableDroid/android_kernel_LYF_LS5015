a86a3ea686d380ae696ac4773afa0c71854da1f0
Viresh Kumar <viresh.kumar@linaro.org>
2013-04-08 16:45:40 +0530

    workqueues: Introduce new flag WQ_POWER_EFFICIENT for power oriented workqueues
    Workqueues can be performance or power-oriented. Currently, most workqueues are
    bound to the CPU they were created on. This gives good performance (due to cache
    effects) at the cost of potentially waking up otherwise idle cores (Idle from
    scheduler's perspective. Which may or may not be physically idle) just to
    process some work. To save power, we can allow the work to be rescheduled on a
    core that is already awake.
    
    Workqueues created with the WQ_UNBOUND flag will allow some power savings.
    However, we don't change the default behaviour of the system.  To enable
    power-saving behaviour, a new config option CONFIG_WQ_POWER_EFFICIENT needs to
    be turned on. This option can also be overridden by the
    workqueue.power_efficient boot parameter.
    
    tj: Updated config description and comments.  Renamed
        CONFIG_WQ_POWER_EFFICIENT to CONFIG_WQ_POWER_EFFICIENT_DEFAULT.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Amit Kucheria <amit.kucheria@linaro.org>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -275,7 +275,6 @@ static bool wq_disable_numa;
 module_param_named(disable_numa, wq_disable_numa, bool, 0444);
 
 /* see the comment above the definition of WQ_POWER_EFFICIENT */
-<<<<<<< HEAD
 #ifdef CONFIG_WQ_POWER_EFFICIENT_DEFAULT
 static bool wq_power_efficient = true;
 #else
@@ -283,11 +282,6 @@ static bool wq_power_efficient;
 #endif
 
 module_param_named(power_efficient, wq_power_efficient, bool, 0444);
-=======
-static bool wq_power_efficient = true;
-
-module_param_named(power_efficient, wq_power_efficient, bool, 0644);
->>>>>>> d3d4129... Add Power-Efficient WorkQueue
 
 static bool wq_numa_enabled;		/* unbound NUMA affinity enabled */
 
