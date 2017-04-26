d3ad7bdefe3ccd55be62b48e044b338a0f2f2d10
Ritesh Saxena <riteshsax007@gmail.com>
2017-04-26 07:08:29 +0000

    Build tools * Initialization from Ritesh Saxena & Build Script from AnyKernel-osm0sis
    * Initial of Nichrome-Kernel for LS-5015
    
    Signed-off-by: Ritesh Saxena <riteshsax007@gmail.com>
    Signed-off-by: Osm0sis  <osm0sis@outlook.com>
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

---
+++ build.sh
@@ -0,0 +1,80 @@
+#
+# Copyright © 2016, Varun Chitre "varun.chitre15" <varun.chitre15@gmail.com>
+# Copyright © 2017, Ritesh Saxena <riteshsax007@gmail.com>
+# Copyright © 2018, Mohd Faraz <mohd.faraz.abc@gmail.com>
+#
+# Custom build script
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# Please maintain this if you use this script or any part of it
+#
+KERNEL_DIR=$PWD
+KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
+BUILD_START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+rm -rf $KERNEL_DIR/out
+mkdir $KERNEL_DIR/out
+# Modify the following variable if you want to build
+export CROSS_COMPILE=$KERNEL_DIR/../uber/bin/aarch64-linux-android-
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_BUILD_USER="Faraz"
+export KBUILD_BUILD_HOST="TimeMachine"
+export USE_CCACHE=1
+BUILD_DIR=$KERNEL_DIR/build
+VERSION="X4"
+DATE=$(date +"%d%m%y")
+
+compile_kernel ()
+{
+echo -e "$blue***********************************************"
+echo "             Compiling Nichrome kernel        "
+echo -e "***********************************************$nocol"
+rm -f $KERN_IMG
+make O=out LS5015_defconfig
+make O=out -j8
+if ! [ -a $KERN_IMG ];
+then
+echo -e "$red Kernel Compilation failed! Fix the errors! $nocol"
+exit 1
+fi
+
+
+make_zip
+}
+
+make_zip ()
+{
+echo "Making Zip"
+rm $BUILD_DIR/*.zip
+rm $BUILD_DIR/zImage
+cp $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb $BUILD_DIR/zImage
+cd $BUILD_DIR
+zip -r Nichrome-$VERSION-$DATE.zip *
+cd $KERNEL_DIR
+}
+
+case $1 in
+clean)
+make ARCH=arm64 O=out-j8 clean mrproper
+rm -rf $KERNEL_DIR/zImage
+;;
+*)
+compile_kernel
+;;
+esac
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
