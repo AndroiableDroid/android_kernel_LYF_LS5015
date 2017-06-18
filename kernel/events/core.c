d90392ce1df23268ccf48e216efda865aaf88b92
Michael Bestas <mikeioannina@gmail.com>
2017-06-19 00:48:00 +0300

    Merge tag 'v3.10.106' into cm-14.1-caf-8916
    This brings LA.BR.1.2.9_rb1.18 up to date with the 3.10.106 stable release.
    
    Conflicts:
    	drivers/media/tuners/tuner-xc2028.c
    	drivers/mfd/pm8921-core.c
    	drivers/video/fbcmap.c
    	fs/9p/acl.c
    	fs/btrfs/acl.c
    	fs/ext2/acl.c
    	fs/ext4/acl.c
    	fs/f2fs/acl.c
    	fs/gfs2/acl.c
    	fs/jfs/xattr.c
    	fs/ocfs2/acl.c
    	fs/posix_acl.c
    	fs/reiserfs/xattr_acl.c
    	fs/xfs/xfs_acl.c
    	include/linux/cpu.h
    	kernel/cpu.c
    	net/ipv4/inet_connection_sock.c
    	net/ipv4/ping.c
    	net/xfrm/xfrm_user.c

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -7656,7 +7656,7 @@ int perf_event_init_context(struct task_struct *child, int ctxn)
 		ret = inherit_task_group(event, parent, parent_ctx,
 					 child, ctxn, &inherited_all);
 		if (ret)
-			break;
+			goto out_unlock;
 	}
 
 	/*
@@ -7672,7 +7672,7 @@ int perf_event_init_context(struct task_struct *child, int ctxn)
 		ret = inherit_task_group(event, parent, parent_ctx,
 					 child, ctxn, &inherited_all);
 		if (ret)
-			break;
+			goto out_unlock;
 	}
 
 	raw_spin_lock_irqsave(&parent_ctx->lock, flags);
@@ -7700,6 +7700,7 @@ int perf_event_init_context(struct task_struct *child, int ctxn)
 	}
 
 	raw_spin_unlock_irqrestore(&parent_ctx->lock, flags);
+out_unlock:
 	mutex_unlock(&parent_ctx->mutex);
 
 	perf_unpin_context(parent_ctx);
