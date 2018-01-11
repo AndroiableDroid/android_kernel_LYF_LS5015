dd69859063bc62ccd95e4e56a131537ecc5ed7c4
Mohd Faraz <Mohd.Faraz.abc@gmail.com>
2018-01-11 12:04:55 +0000

    core.c : Fixed Compilation

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -1020,7 +1020,6 @@ int device_add(struct device *dev)
 	struct class_interface *class_intf;
 	struct kobject *glue_dir = NULL;
 	int error = -EINVAL;
-	struct kobject *glue_dir = NULL;
 
 	dev = get_device(dev);
 	if (!dev)
